970a1a5936769502a279c191493a81c9792fa418
TJ Littlejohn <littlejohn.tj@gmail.com>
2017-02-03 12:20:20 -0500

    Initial Commit

---
+++ JukeboxApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JukeboxApp
+//
+//  Created by Todd Littlejohn on 2/3/17.
+//  Copyright © 2017 TKC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JukeboxApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JukeboxApp
+//
+//  Created by Todd Littlejohn on 2/3/17.
+//  Copyright © 2017 TKC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
